63417dadab7cc6b8d75f9076f590997c5c439f02
ochoaluis_cepi <ochoaluis_cepi@hotmail.com>
2024-03-06 14:56:25 -0600

    Lift up

--- Resume/obj/Debug/net8.0/Resume.AssemblyInfo.cs
+++ Resume/obj/Debug/net8.0/Resume.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f579cee7913455e4ae509b0af27132ff8af91dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
